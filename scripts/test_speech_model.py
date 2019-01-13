8657f03a7d1fe6ba026ebfb3e99be297452efacc
guichristmann <guichristmann@gmail.com>
2019-01-13 19:19:21 +0000

    Updated test_speech_model.py

--- scripts/test_speech_model.py
+++ scripts/test_speech_model.py
@@ -5,26 +5,39 @@ sys.path.append('speech_detection/')
 from speech_detector import SpeechDetection
 from datetime import datetime
 
-if len(sys.argv) != 2:
-    print("Usage: python " + sys.argv[0] + " <speech_model>")
-    sys.exit(0)
-
-detect_count = 0
-def callback():
-    global detect_count
+yes_count = 0
+no_count = 0
+isit_count = 0
+def YesCallback():
+    global yes_count
     
-    detect_count += 1
+    yes_count += 1
+
+def NoCallback():
+    global no_count
+
+    no_count += 1
+
+def IsItCallback():
+    global isit_count
+
+    isit_count += 1
 
 def getTimestamp():
     return str(datetime.now()).split('.')[0]
 
-model = SpeechDetection(sys.argv[1],
-                        wake_callback=[callback],
-                        audio_gain=0.95,
-                        sensitivity=0.5)
+#models = SpeechDetection(["speech_models/Yes.pmdl", "speech_models/No.pmdl"],
+#                            wake_callback=[YesCallback, NoCallback],
+#                            audio_gain=0.95,
+#                            sensitivity=0.4)
+
+isit_model = SpeechDetection("speech_models/Is_it.pmdl",
+                            wake_callback=[IsItCallback],
+                            audio_gain=1.0,
+                            sensitivity=0.5)
 
 print("Start time: " + getTimestamp())
 while True:
-    model.run(process=False)
-    print("Detect count: " + str(detect_count))
+    isit_model.run(process=False)
+    print('"Is It" count: ' + str(isit_count))
     print("Current timestamp: " + getTimestamp())
